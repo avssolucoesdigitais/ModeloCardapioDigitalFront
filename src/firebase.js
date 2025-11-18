cae1d2f5e9b699a57612a2c24d723174b2a11fcc
San096 <sanderleycc2022@alu.ufc.br>
2025-11-18 20:43:12 -0300

    segurança 3

--- src/firebase.js
+++ src/firebase.js
@@ -2,18 +2,19 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
-  console.log("firebaseConfig:", firebaseConfig);
 const firebaseConfig = {
-  apiKey: "AIzaSyBpBVNv93rvcEe-brq-8F-sdczhZ5Agmtw",
-  authDomain: "cardapio-c5695.firebaseapp.com",
-  projectId: "cardapio-c5695",
-  storageBucket: "cardapio-c5695.firebasestorage.app",
-  messagingSenderId: "733934320699",
-  appId: "1:733934320699:web:5b8eb2156145327e08701e"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+
+console.log("firebaseConfig:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export default app;
